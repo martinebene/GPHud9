248fa3ef67503079e22d127ddda2581526f0cd3d
martinebene <martinebene@gmail.com>
2015-02-01 16:31:30 -0300

    Initial commit

---
+++ app/src/main/java/com/mebene/GPHud/Fm_control.java
@@ -0,0 +1,22 @@
+package com.mebene.GPHud;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 26/08/2014.
+ */
+public class Fm_control extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.seccion1, container, false);
+
+        return rootView;
+    }
+}
